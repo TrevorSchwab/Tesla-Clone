7408135bbb27fb8ddaefcdbba6a0f4011e10f9f9
Trevor Schwab <trevschwab@gmail.com>
2021-04-24 15:45:48 -0600

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View} from 'react-native';
+import CarItem from './components/CarItem/index';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CarItem />
     </View>
   );
 }
@@ -17,5 +17,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
